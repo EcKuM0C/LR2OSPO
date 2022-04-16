8ea886dba9253ddb1c76b84f6778556086a68357
EcKuM0C <ermik2001@mail.ru>
2022-04-16 16:57:34 +0300

    first

---
+++ 34234.bat
@@ -0,0 +1,31 @@
+echo off
+md Astapenya
+cd Astapenya
+md Ermolay
+cd Ermolay
+md Alexandrovich
+cd ..
+cd ..
+pause
+echo off
+cd Astapenya
+echo > 08082001.txt
+cd Ermolay
+cd Alexandrovich
+echo > M400009494.txt
+pause
+echo off
+cd ..
+cd ..
+cd ..
+del Astapenya /S /Q /F
+pause
+echo off
+cd Astapenya
+cd Ermolay
+rd Alexandrovich
+cd ..
+rd Ermolay
+cd ..
+rd Astapenya
+pause
